a9a09f2a24abe92b115196b926af9c0d89cf9b0a
Marie Pendley <mariependley@gmail.com>
2019-01-26 20:27:05 -0500

    added friend hill script

---
+++ Assets/Scripts/Rounds/FriendHill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendHill : RoundAbstract {
+
+    private List<int> alivePlayers;
+
+    override public void StartRound() {
+        RoundManager.instance.SetTimer(10f, EndRound);
+        RoundManager.instance.onPlayerDeath += OnPlayerDeath;
+        alivePlayers = GameManager.CreateIndexList();
+    }
+
+    public void OnPlayerDeath(int f) {
+    }
+
+    override public void EndRound() {
+        RoundManager.instance.onPlayerDeath -= OnPlayerDeath;
+        RoundManager.instance.NextRound();
+    }
+}
